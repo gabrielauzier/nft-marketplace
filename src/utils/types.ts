a23d9e318cd5301442fd14c2922b8f8452b56458
gabvask <gabrielvascoauzier@gmail.com>
2022-05-11 01:44:38 -0400

    chore: add types

---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+export type RootParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+export type Item = {
+  id: string;
+  name: string;
+  creator: string;
+  price: number;
+  description: string;
+  imageURL: any;
+  bids: Bid[];
+};
+
+export type Bid = {
+  id: string;
+  name: string;
+  price: number;
+  imageURL: any;
+  date: string;
+};
