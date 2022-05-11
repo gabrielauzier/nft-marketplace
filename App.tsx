4f16ff6ff630fe2f92dd45c1afdbda9c04b11771
gabvask <gabrielvascoauzier@gmail.com>
2022-05-11 01:44:30 -0400

    chore: configure navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import React, { ProviderProps } from "react";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+  StackScreenProps,
+} from "@react-navigation/stack";
+import {
+  DefaultTheme,
+  NavigationContainer,
+  NavigationContainerProps,
+  StackActions,
+} from "@react-navigation/native";
+import { RootParamList } from "./src/utils/types";
+import Home from "./src/screens/Home";
+import Details from "./src/screens/Details";
+import { useFonts } from "expo-font";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  },
+};
+
+const Stack = createStackNavigator<RootParamList>();
 
 export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if (!loaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        <Stack.Screen name="Details" component={Details}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
